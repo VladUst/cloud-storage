fffb478606af330f8e7361fe3134adea919c7d9b
VladUs <vladustyugo@gmail.com>
2022-08-07 16:08:08 +0700

    file download API created

--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -4,6 +4,7 @@ import dirLogo from '../../../../assets/img/dir-logo.svg';
 import fileLogo from '../../../../assets/img/file-logo.svg';
 import {useDispatch, useSelector} from "react-redux";
 import {pushToStack, setCurrentDir} from "../../../../reducers/fileReducer";
+import {downloadFile} from "../../../../actions/file";
 const File = ({file}) => {
     const dispatch = useDispatch();
     const currentDir = useSelector(state => state.files.currentDir)
@@ -14,12 +15,19 @@ const File = ({file}) => {
         }
     }
 
+    function downloadHandler(e) {
+        e.stopPropagation();
+        downloadFile(file);
+    }
+
     return (
         <div className='file' onClick={() => openDirHandler()}>
             <img src={file.type === 'dir' ? dirLogo : fileLogo } alt="" className='file__icon'/>
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0,10)}</div>
             <div className="file__size">{file.size}</div>
+            {file.type !=='dir' && <button onClick={downloadHandler} className="file__btn file__download">download</button>}
+            <button className="file__btn file__delete">delete</button>
         </div>
     );
 };
--- server/routes/file.router.js
+++ server/routes/file.router.js
@@ -4,7 +4,8 @@ const authMiddleware = require('../middleware/auth.middleware');
 const fileController = require('../controllers/fileController');
 
 router.post('', authMiddleware, fileController.createDir);
-router.post('/upload', authMiddleware, fileController.uploadFile);
 router.get('', authMiddleware, fileController.getFiles);
+router.post('/upload', authMiddleware, fileController.uploadFile);
+router.get('/download', authMiddleware, fileController.downloadFile);
 
 module.exports = router
