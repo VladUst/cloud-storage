ec3e2ec5befbfda9d3146cec6577c04084740049
VladUs <vladustyugo@gmail.com>
2022-08-09 15:05:27 +0700

    sorting created

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -2,11 +2,20 @@ import axios from "axios";
 import {addFile, deleteFileAction, setFiles} from "../reducers/fileReducer";
 import {addUploadFile, changeUploadFile, showUploader} from "../reducers/uploadReducer";
 
-export function getFiles(dirId) {
+export function getFiles(dirId, sort) {
     return async dispatch => {
         try{
-            console.log(`http://localhost:5000/api/files${dirId ? '?parent='+dirId : ''}`);
-            const response = await axios.get(`http://localhost:5000/api/files${dirId ? '?parent='+dirId : ''}`, {
+            let url = `http://localhost:5000/api/files`;
+            if(dirId){
+                url = `http://localhost:5000/api/files?parent=${dirId}`;
+            }
+            if(sort){
+                url = `http://localhost:5000/api/files?sort=${sort}`;
+            }
+            if(dirId && sort){
+                url = `http://localhost:5000/api/files?parent=${dirId}&sort=${sort}`;
+            }
+            const response = await axios.get(url, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
             });
             dispatch(setFiles(response.data))
--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -11,9 +11,10 @@ const Disk = () => {
     const currentDir = useSelector(state=>state.files.currentDir);
     const dirStack = useSelector(state => state.files.dirStack);
     const [dragEnter, setDragEnter] = useState(false);
+    const [sort, setSort] = useState('type');
     useEffect(()=>{
-        dispatch(getFiles(currentDir))
-    }, [currentDir])
+        dispatch(getFiles(currentDir, sort))
+    }, [currentDir, sort])
 
     function showPopup() {
         dispatch(setPopupDisplay('flex'));
@@ -59,6 +60,11 @@ const Disk = () => {
                     <label htmlFor="disk__upload-input" className="disk__upload-label">Загрузить файл</label>
                     <input multiple={true} onChange={(e)=>fileUploadHandler(e)} type="file" id="disk__upload-input" className='disk__upload-input'/>
                 </div>
+                <select value={sort} onChange={(e)=>setSort(e.target.value)} className='disk__select'>
+                    <option value="name">По имени</option>
+                    <option value="type">По типу</option>
+                    <option value="date">По дате</option>
+                </select>
             </div>
             <FileList/>
             <Popup/>
